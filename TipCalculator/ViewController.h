9f66917ebcb7374ef892050fd110f7437dea67c6
victorhong1988 <victorhong1988@gmail.com>
2016-11-11 10:38:20 -0500

    Initial Commit

---
+++ TipCalculator/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TipCalculator
+//
+//  Created by Victor Hong on 11/11/2016.
+//  Copyright © 2016 Victor Hong. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
